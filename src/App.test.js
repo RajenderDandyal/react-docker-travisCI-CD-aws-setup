8f52d6c72609f5c6f09e123fe84135e00535f37d
RajenderDandyal <rajender0184@gmail.com>
2019-07-31 00:53:24 +0530

    updt

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,23 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
+
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<App />);
+  const welcome = <p>Hi there!! how r u?</p>;
+  // expect(wrapper.contains(welcome)).toBe(true);
+  expect(wrapper.contains(welcome)).toEqual(true)
 });
+
 it('renders without crashing', () => {
+  expect(2).toBe(2)
+});
+it('renders without crashing2', () => {
+  expect(2 + 2).toBe(4);
+});
+it('renders without crashing3', () => {
   expect(2 + 2).toBe(4);
 });
